1636e47f16f624cfda73e1e51f19fc17b979db97
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-03 12:25:36 -0700

    Adds Logo Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import Logo from './logo'
 
 const App = props => {
   
   return (
     <div className='app'>
-      Logo
+      <Logo />
     </div>
   )
 }
---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+
+const Logo = props => {
+  
+
+  return (
+    <div className='Logo'>
+      <img alt="Daily Smarty Logo" src="/assets/ds_circle_logo.png" />
+    </div>
+  )
+}
+
+export default Logo
